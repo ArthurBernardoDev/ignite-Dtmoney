d870ddd4a59f39d81a20fb6f353926b22234ab08
Arthur bernardo <arthurutschgg@gmail.com>
2021-07-13 10:35:41 -0300

    consumindo api com miragejs

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,33 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
 export function Header() {
+
+    const [isnewTransactionModalOpen, setIsnewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsnewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsnewTransactionModalOpen(false);
+    }
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="Logo" />
-                <button type="button">
+                <button type="button" onClick={handleOpenNewTransactionModal}>
                     Nova transação
                 </button>
+
+                <Modal isOpen={isnewTransactionModalOpen}
+                    onRequestClose={handleCloseNewTransactionModal}
+                >
+                    <h2>Cadastrar transação</h2>
+                </Modal>
             </Content>
         </Container>
     )
